d5cb8f523e0b2ac58f67008855d63c86b3f35f0c
Radik Zangirov <owtmpatrick@gmail.com>
2021-11-16 10:37:29 +0500

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, './client/build')));
+
+app.get('/articles', (req, res) => {
+	res.send({articles: [{name: 'first article'}]});
+});
+
+// app.get('*', (req, res) => {
+// 	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
+
+app.listen(port, () => {
+	console.log(`App listening on port ${port}`);
+});
